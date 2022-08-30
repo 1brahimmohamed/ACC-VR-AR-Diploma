a5c5a25e04f9d899308a7e4e583e97e6809c7ea5
Ibrahim <imimazim7@gmail.com>
2022-08-30 23:11:33 +0200

    added tasks

---
+++ Tasks/Week 4/Assets/Scripts/FollowPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPoint : MonoBehaviour
+{
+    public GameObject followPoint;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, followPoint.transform.position)>0.20f)
+        {
+            transform.LookAt(new Vector3(followPoint.transform.position.x,0.28f,followPoint.transform.position.z));
+            transform.position += transform.forward * 0.9f * Time.deltaTime;
+            anim.SetBool("walking",true); 
+        }
+        else
+        {
+            anim.SetBool("walking",false);
+        }
+    }
+}
---
+++ Tasks/Week 4/Assets/Scripts/SendRay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendRay : MonoBehaviour
+{
+    public GameObject followPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray landingRay = new Ray(transform.position, transform.forward);
+
+        if(OVRInput.GetDown(OVRInput.Button.One))
+        {
+            Debug.Log("Pressed");
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitInfo,20f))
+            {
+                    followPoint.transform.position = hitInfo.point;
+            }
+        }
+    }
+
+}
